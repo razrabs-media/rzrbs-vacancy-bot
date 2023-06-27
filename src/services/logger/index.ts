02f74f533228d31f170027538ed43ab04896e6d5
Mariia Lobareva <marialobareva97@gmail.com>
2023-06-27 21:30:37 +0200

    chore: adds console logs to prod

--- src/services/logger/index.ts
+++ src/services/logger/index.ts
@@ -16,18 +16,18 @@ const logger = winston.createLogger({
     )
   ),
   transports: [
-    //
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: "debug.log", level: "debug" }),
     new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.Console(),
   ],
 });
 
 if (config.environment !== Environment.Prod) {
-  logger.add(new winston.transports.Console());
+  logger.add(
+    new winston.transports.File({ filename: "debug.log", level: "debug" })
+  );
 }
 
 logger.info.bind(logger);
