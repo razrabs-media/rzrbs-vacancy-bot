13e212e8e79b41c65a1e71ce06234faa3c523a34
marylorian <marialobareva97@gmail.com>
2023-08-10 13:40:17 +0200

    issue(40): fixes tests and date format

--- src/utils/__tests__/getVacancyWillBePublishedText.test.ts
+++ src/utils/__tests__/getVacancyWillBePublishedText.test.ts
@@ -4,7 +4,7 @@ describe("getVacancyWillBePublishedText", () => {
   it("should return string with date", () => {
     const mockDate = new Date(1466424490000);
     expect(getVacancyWillBePublishedText(mockDate)).toBe(
-      "Вакансия будет опубликована примерно: 6/20/2016, 2:08:10 PM"
+      "Вакансия будет опубликована примерно: 20 июня 2016 г. в 14:08"
     );
   });
 });
--- src/utils/getVacancyWillBePublishedText.ts
+++ src/utils/getVacancyWillBePublishedText.ts
@@ -1,5 +1,10 @@
 import { VACANCY_WILL_BE_PUBLISHED } from "../constants/labels";
 
-export const getVacancyWillBePublishedText = (dateTime: Date): string => {
-  return `${VACANCY_WILL_BE_PUBLISHED}: ${dateTime.toLocaleString()}`;
-};
+export const getVacancyWillBePublishedText = (date: Date): string =>
+  `${VACANCY_WILL_BE_PUBLISHED}: ${date.toLocaleDateString("ru-RU", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  })}`;
